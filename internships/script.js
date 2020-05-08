42d418d54291d42903195dd8bfd5ca24f586fcfc
benson <bensontwu@benson-mbp.attlocal.net>
2020-05-08 15:06:02 -0700

    removed old script

--- internships/script.js
+++ internships/script.js
@@ -4,6 +4,80 @@ const fs = require('fs');
 const pug = require('pug');
 const pretty = require('pretty');
 
+var industry_groupings = [
+    {
+        'name': 'af',
+        'grouping': ["Accounting", "Finance"]
+    },
+    {
+        'name': 'ms',
+        'grouping': ["Marketing", "Sales"]
+    },
+    {
+        'name': 'co',
+        'grouping': ["Consulting", "Operations"]
+    },
+    {
+        'name': 'ha',
+        'grouping': ["Human Resources", "Administration"]
+    },
+    {
+        'name': 'tp',
+        'grouping': ["Software", "Product Management", "Technology"]
+    }
+];
+
+class InternshipTable {
+    constructor(industry_groupings) {
+        this.industries = {};
+        for (var grouping_obj of industry_groupings) {
+            this.industries[grouping_obj['name']] = [];
+        }
+        this.key = industry_groupings;
+    }
+    push(record) {
+        console.log(record['Active']);
+        if (record['Active'] == null) {
+            return // Do nothing on empty records
+        }
+        // Get the industry obj that the record belongs to
+        var industry_obj = this.getIndustry(record['Industry']);
+        var year_obj = this.getYear(industry_obj, record['Year']);
+        if (!year_obj) {
+            // Set year_obj if it is null
+            year_obj = {
+                "year": record['Year'],
+                "records": []
+            };
+            industry_obj.push(year_obj);
+            industry_obj.sort((a,b) => b["year"]-a["year"]);
+        }
+        year_obj['records'].push(record);
+    }
+    getIndustry(industry_name) {
+        // Get the industry obj that the record belongs to
+        for (var grouping_obj of this.key) {
+            for (var group_item of grouping_obj['grouping']) {
+                if (industry_name == group_item) {
+                    return this.industries[grouping_obj['name']];
+                }
+            }
+        }
+        return null;
+    }
+    getYear(industry_obj, year_name) {
+        // Loop through the years
+        for (var year of industry_obj) {
+            // Attempt to find the year object that matches the one in the record
+            if (year['year'] == year_name) return year;
+        }
+        // Return null if not found
+        return null;    
+    }
+}
+
+// Store records in the table json data struction
+var table = new InternshipTable(industry_groupings);
 
 // Configurations
 dotenv.config();
@@ -14,31 +88,8 @@ base('Internships').select({
 }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
-    // Store records in the table json data struction
-    var table = new InternshipTable();
-
     records.forEach(function(record) {
-    	table.push(record.fields);
-    });
-
-
-    // Write the HTML file
-    const compiledFunction = pug.compileFile('template.pug');
-    html_str = compiledFunction(table);
-    fs.writeFile('output.html', pretty(html_str), (err) => {
-    	// Catch error
-    	if (err) throw err;
-
-    	console.log('HTML file created!');
-    });
-
-    // Write the JSON file
-    var json_str = JSON.stringify(table, null, 2);
-    fs.writeFile('output.json', json_str, (err) => {
-    	// Catch error
-    	if (err) throw err;
-
-    	console.log('JSON file created!');
+        table.push(record.fields);
     });
 
     // To fetch the next page of records, call `fetchNextPage`.
@@ -48,54 +99,31 @@ base('Internships').select({
 
 }, function done(err) {
     if (err) { console.error(err); return; }
+
+    writeHTML()
+    writeJSON()
+
 });
 
+function writeHTML() {
+    // Write the HTML file
+    const compiledFunction = pug.compileFile('template.pug');
+    html_str = compiledFunction(table);
+    fs.writeFile('output.html', pretty(html_str), (err) => {
+        // Catch error
+        if (err) throw err;
 
-
-
-class InternshipTable {
-	constructor() {
-		this.industries = {
-            "Marketing": [],
-            "Software": [],
-            "Consulting": [],
-            "Accounting": [],
-            "Operations": [],
-            "Finance": [],
-            "Human Resources": [],
-            "Sales": [],
-            "Product Management": [],
-            "Administration": []
-        }
-	}
-	push(record) {
-		// Get the industry obj that the record belongs to
-    	var industry_obj = this.industries[record['Industry'][0]];
-    	var year_obj = this.getYear(industry_obj, record['Year']);
-    	if (!year_obj) {
-    		// Set year_obj if it is null
-    		year_obj = {
-    			"year": record['Year'],
-    			"records": []
-    		};
-    		industry_obj.push(year_obj);
-    	}
-    	year_obj['records'].push(record);
-	}
-	getYear(industry_obj, year_name) {
-		// Loop through the years
-		for (var i = 0; i < industry_obj.length; i++) {
-			// Attempt to find the year object that matches the one in the record
-			if (industry_obj[i]['year'] == year_name) return industry_obj[i];
-		}
-		// Return null if not found
-		return null;	
-	}
+        console.log('HTML file created!');
+    });
 }
 
+function writeJSON() {
+    // Write the JSON file
+    var json_str = JSON.stringify(table, null, 2);
+    fs.writeFile('output.json', json_str, (err) => {
+        // Catch error
+        if (err) throw err;
 
-function createHTML(table) {
-	var template = {}
+        console.log('JSON file created!');
+    });
 }
-
-
